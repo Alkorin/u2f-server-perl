247a5e21effb6207cb28fc3c8e55b38b361d32b0
Alkorin <github@alkorin.fr>
2014-12-25 14:49:42 +0100

    Add script which generates the challenge request

---
+++ generateRegister.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Getopt::Long;
+use JSON;
+use MIME::Base64 qw{ encode_base64url };
+
+# Parse options
+my $appId;
+GetOptions("appId|a=s" => \$appId);
+if(!$appId)
+{
+	print STDERR "--appId ('-a') option required\n";
+	exit -1;
+}
+
+# Create challenge
+open(FH, '/dev/urandom');
+read(FH, my $challenge, 32);
+close FH;
+
+# Output result
+print encode_json({
+	challenge => encode_base64url($challenge),
+	version   => "U2F_V2",
+	appId     => $appId
+});
